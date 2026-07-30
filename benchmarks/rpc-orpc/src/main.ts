1070c9ae2350f525356d12d448a8a7c6ddac44f7
Dinh Le <dinwwwh@gmail.com>
2026-07-30 11:06:30 +0700

    upgrade & orpc v2

--- benchmarks/rpc-orpc/src/main.ts
+++ benchmarks/rpc-orpc/src/main.ts
@@ -1,42 +1,66 @@
-import { createServer } from 'node:http'
-import { os } from '@orpc/server'
-import { RPCHandler } from '@orpc/server/node'
+import { createServer } from "node:http";
+import { os } from "@orpc/server";
+import { RPCHandler } from "@orpc/server/node";
 
 const schema = {
-  '~standard': {
-    vendor: 'custom',
+  "~standard": {
+    vendor: "custom",
     validate: (value: any) => ({ value }),
     version: 1,
   },
-} as const
+} as const;
 
 const router = {
-  procedure_1: os.input(schema).handler(({ input }) => ({ name: 'procedure_1', date: new Date(), input })),
-  procedure_2: os.input(schema).handler(({ input }) => ({ name: 'procedure_2', date: new Date(), input })),
+  procedure_1: os
+    .input(schema)
+    .handler(({ input }) => ({ name: "procedure_1", date: new Date(), input })),
+  procedure_2: os
+    .input(schema)
+    .handler(({ input }) => ({ name: "procedure_2", date: new Date(), input })),
   nested: {
-    procedure_1: os.input(schema).handler(({ input }) => ({ name: 'nested/procedure_1', date: new Date(), input })),
-    procedure_2: os.input(schema).handler(({ input }) => ({ name: 'nested/procedure_2', date: new Date(), input })),
+    procedure_1: os.input(schema).handler(({ input }) => ({
+      name: "nested/procedure_1",
+      date: new Date(),
+      input,
+    })),
+    procedure_2: os.input(schema).handler(({ input }) => ({
+      name: "nested/procedure_2",
+      date: new Date(),
+      input,
+    })),
     nested: {
-      procedure_1: os.input(schema).handler(({ input }) => ({ name: 'nested/nested/procedure_1', date: new Date(), input })),
-      procedure_2: os.input(schema).handler(({ input }) => ({ name: 'nested/nested/procedure_2', date: new Date(), input })),
+      procedure_1: os.input(schema).handler(({ input }) => ({
+        name: "nested/nested/procedure_1",
+        date: new Date(),
+        input,
+      })),
+      procedure_2: os.input(schema).handler(({ input }) => ({
+        name: "nested/nested/procedure_2",
+        date: new Date(),
+        input,
+      })),
     },
   },
-}
+};
 
 const handler = new RPCHandler(router, {
-  strictGetMethodPluginEnabled: false,
-})
+  csrfGuardPlugin: {
+    enabled: false,
+  },
+});
 
 const server = createServer(async (req, res) => {
-  const { matched } = await handler.handle(req, res)
+  const { matched } = await handler.handle(req, res);
 
   if (!matched) {
-    res.statusCode = 404
-    res.end('Not Found')
+    res.statusCode = 404;
+    res.end("Not Found");
   }
-})
+});
 
-server.listen(3000, '127.0.0.1', () => {
+server.listen(3000, "127.0.0.1", () => {
   // eslint-disable-next-line no-console
-  console.log('Listening on http://127.0.0.1:3000/nested/procedure_1?data=%7B%22json%22%3A%22hello%20world%22%2C%22meta%22%3A%5B%5D%7D')
-})
+  console.log(
+    "Listening on http://127.0.0.1:3000/nested/procedure_1?data=%7B%22json%22%3A%22hello%20world%22%2C%22meta%22%3A%5B%5D%7D",
+  );
+});
--- benchmarks/typecheck-orpc/src/client.ts
+++ benchmarks/typecheck-orpc/src/client.ts
@@ -1,10 +1,8 @@
-import type { RouterClient } from '@orpc/server'
-import type { router } from './router'
-import { createORPCClient } from '@orpc/client'
-import { RPCLink } from '@orpc/client/fetch'
+import type { RouterClient } from "@orpc/server";
+import type { router } from "./router";
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
 
-const link = new RPCLink({
-  url: 'https://example.com/rpc',
-})
+const link = new RPCLink({});
 
-export const client: RouterClient<typeof router> = createORPCClient(link)
+export const client: RouterClient<typeof router> = createORPCClient(link);
